45134be9e48499bfbd9b7e2e133ea87befb3ba64
AnhTuanPham1507 <62019976+AnhTuanPham1507@users.noreply.github.com>
2021-10-27 20:42:20 +0700

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -165,8 +165,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '%s/EcommerceApp/' % BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'EcommerceApp')
 MEDIA_ROOT = '%s/EcommerceApp/media' % BASE_DIR
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
